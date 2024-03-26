7a738afde334841c4d1ad83cdfbc3e13f9ead889
Noor muhammed <noormuhammed12345678@gmail.com>
2024-03-26 10:39:33 +0530

    created feature and modified cart.php

--- src/Facades/Cart.php
+++ src/Facades/Cart.php
@@ -1,16 +1,67 @@
 <?php
-namespace Gloudemans\Shoppingcart\Facades;
 
-use Illuminate\Support\Facades\Facade;
+return [
 
-class Cart extends Facade {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor()
-    {
-        return 'cart';
-    }
-}
+    /*
+    |--------------------------------------------------------------------------
+    | Default tax rate
+    |--------------------------------------------------------------------------
+    |
+    | This default tax rate will be used when you make a class implement the
+    | Taxable interface and use the HasTax trait.
+    |
+    */
+
+    'tax' => 21,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Shoppingcart database settings
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set the connection that the shoppingcart should use when
+    | storing and restoring a cart.
+    |
+    */
+
+    'database' => [
+
+        'connection' => null,
+
+        'table' => 'shoppingcart',
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Destroy the cart on user logout
+    |--------------------------------------------------------------------------
+    |
+    | When this option is set to 'true' the cart will automatically
+    | destroy all cart instances when the user logs out.
+    |
+    */
+
+    'destroy_on_logout' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default number format
+    |--------------------------------------------------------------------------
+    |
+    | This defaults will be used for the formated numbers if you don't
+    | set them in the method call.
+    |
+    */
+
+    'format' => [
+
+        'decimals' => 2,
+
+        'decimal_point' => '.',
+
+        'thousand_seperator' => ','
+
+    ],
+
+];
